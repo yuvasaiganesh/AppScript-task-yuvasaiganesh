5b6acfbfbac7d99617d507ce0112bd48b3db11ab
yuvasaiganesh <yuvasaiganesh@gmail.com>
2024-04-21 13:55:24 +0530

    helemt elemnet added

--- src/App.js
+++ src/App.js
@@ -4,13 +4,19 @@
     import Navbar from './components/Navbar';
     import "./App.css"
     import Footer from './components/Footer';
+    import {Helmet} from "react-helmet";
     
     
     
     
     
     const App = () => (
-      <div className='app'><Navbar />
+      <div className='app'>
+        <Helmet>
+            <title>AppScript Home Page</title>
+            <meta name="description" content="Home & Products Component" />
+        </Helmet>
+        <Navbar />
       <ProductsGrid />
       <Footer/>
       </div>
